5b08e0fac1887fbdaa7660514a048b46988d9300
CeciliaFerrer <gisela-cecilia.ferrer-parise.external@capgemini.com>
2024-05-27 10:18:56 +0200

    proyecto completo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { AuthorListComponent } from './author/author-list/author-list.component';
+import { GameListComponent } from './game/game-list/game-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/games', pathMatch: 'full'},
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'authors', component: AuthorListComponent},
+  { path: 'games', component: GameListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+/**{
+  path: 'customers'
+  loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)
+}**/
